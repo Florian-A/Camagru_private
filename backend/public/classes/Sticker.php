48b3bfdd68a35d18e6277985629519c7420b519d
Florian <git@florian-a.dev>
2024-06-19 20:02:08 +0200

    cay bo

--- backend/public/classes/Sticker.php
+++ backend/public/classes/Sticker.php
@@ -8,7 +8,13 @@ class Sticker
             $stmt = $pdo->query('SELECT * FROM sticker');
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
             if ($result !== false) {
+                foreach ($result as &$sticker) {
+                    if (isset($sticker['imagePath'])) {
+                        $sticker['imagePath'] = './static/' . $sticker['imagePath'];
+                    }
+                }
                 return ["status" => "success", "data" => $result];
             } else {
                 return ["status" => "error", "message" => "No stickers found"];
