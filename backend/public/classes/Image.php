35d5eeb55a4792b64badf43cf803be7e20a9c291
Florian <git@florian-a.dev>
2024-06-20 17:02:09 +0200

    mwou

--- backend/public/classes/Image.php
+++ backend/public/classes/Image.php
@@ -11,6 +11,7 @@ class Image
         if ($data && isset($data->image)) {
             // Récupérer les données de l'image
             $imageData = $data->image;
+            $stickersId = $data->stickersId;
     
             // Supprimer le schéma de données de l'URL (si présent)
             $imageData = str_replace('data:image/png;base64,', '', $imageData);
@@ -20,7 +21,7 @@ class Image
             $imageBinary = base64_decode($imageData);
     
             // Chemin où sauvegarder l'image (assurez-vous que le répertoire est accessible en écriture)
-            $filePath = './'; // Adapter le chemin selon votre configuration
+            $filePath = './static/'; // Adapter le chemin selon votre configuration
     
             // Nom de fichier pour l'image (peut être généré de manière unique si nécessaire)
             $fileName = 'image_' . uniqid() . '.png'; // Adapter l'extension selon le type MIME de l'image
@@ -32,7 +33,9 @@ class Image
             $bytesWritten = file_put_contents($fullPath, $imageBinary);
     
             if ($bytesWritten !== false) {
-                echo "L'image a été sauvegardée avec succès : $fullPath";
+                echo "imageData : $fullPath";
+                echo "stickerId : ";
+                echo implode(',', $stickersId);
             } else {
                 echo "Erreur lors de la sauvegarde de l'image.";
             }
